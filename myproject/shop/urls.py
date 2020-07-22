30698458916019b25f7e57db404fd0172ed9ab4f
InKyeongBae <hwang0809@cau.ac.kr>
2020-07-23 04:01:10 +0900

    6다양한 응답의 함수 기반 뷰 만들기

--- myproject/shop/urls.py
+++ myproject/shop/urls.py
@@ -1,10 +1,23 @@
-from django.urls import path,register_converter
+from django.urls import path,register_converter,re_path
 from .converters import FourDigitYearConverter
 from . import views
+from .views import item_list
 
 app_name='shop'
 
 register_converter(FourDigitYearConverter,'yyyy')
 urlpatterns = [
-    path('archives/<yyyy:year>/',views.archives_year)
+    #
+    # path('', views.item_list, name='item_list'),                           # Item 목록
+    # path('new/', views.item_new, name='item_new'),                         # 새 Item
+    # path('<int:id>/', views.item_detail, name='item_detail'),              # Item 보기
+    # re_path(r'^(?P<id>\d+)/$', views.item_detail, name='item_detail'),     # 혹은 re_path 활용
+    # path('<int:id>/edit/', views.item_edit, name='item_edit'),             # Item 수정
+    # path('<int:id>/delete/', views.item_delete, name='item_delete'),       # Item 삭제
+    # path('<int:id>/reviews/', views.review_list, name='review_list'),      # 리뷰 목록
+    # path('<int:item_id>/reviews/<int:id>/edit/$', views.review_edit, name='review_edit'),
+    # path('<int:item_id>/reviews/<int:id>/delete/$', views.review_delete, name='review_delete'),
+    path('items/',item_list,name='item_list'),
+    path('shop/',views.item_list),
+    path('archives/<yyyy:year>/',views.archives_year),
 ]
--- myproject/shop/views.py
+++ myproject/shop/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Item
 # Create your views here.
+
+def item_list(request):
+    qs=Item.objects.all()
+    return render(request,'shop/Item_list.html',{
+        'item_list':qs,
+    })
 
 def archives_year(request,year):
     return HttpResponse('{}년도에 대한 내용'.format(year))
