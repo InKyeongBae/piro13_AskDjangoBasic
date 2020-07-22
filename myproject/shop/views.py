00dfb1acfaf25adc279033c577b03b1221aecbe8
InKyeongBae <hwang0809@cau.ac.kr>
2020-07-23 04:14:59 +0900

    7적절한 HTTP상태 ì½코드로 ì응답하기

--- myproject/shop/views.py
+++ myproject/shop/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+
 from .models import Item
 # Create your views here.
+from django.http import HttpResponseRedirect,HttpResponse
+from django.shortcuts import redirect, resolve_url, render
+def view1(request): return HttpResponseRedirect('/shop/')
+def view2(request):
+    url = resolve_url('shop:item_list')
+    return HttpResponseRedirect(url)
+def view3(request):
+    return redirect('shop:item_list')
+
+def test_view(request):
+    return HttpResponse(status=201)
+
+
 
 def item_list(request):
     qs=Item.objects.all()
