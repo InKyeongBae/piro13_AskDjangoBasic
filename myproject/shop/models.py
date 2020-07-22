64c2f8f347c4cc3051f03d1338e293fcb012ca58
InKyeongBae <hwang0809@cau.ac.kr>
2020-07-23 06:57:31 +0900

    14모델을 통한 데이터 생성/수정/삭제

--- myproject/shop/models.py
+++ myproject/shop/models.py
@@ -8,6 +8,7 @@ class Item(models.Model):
     price=models.PositiveIntegerField()
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return f'<{self.pk}>{self.name}'
 
