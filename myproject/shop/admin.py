ef7fa26885576ab037b5d0d94a4ea23da186a6ba
InKyeongBae <hwang0809@cau.ac.kr>
2020-07-23 06:21:27 +0900

    12장고 Admin을 통한 데이터 관리

--- myproject/shop/admin.py
+++ myproject/shop/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Item
 # Register your models here.
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display=['pk','name','short_desc','price']
+    list_display_links = ['name']
+    search_fields = ['name']
+    list_filter = ['updated_at']
+
+    def short_desc(self,item):
+        return item.desc[:20]
+    pass
--- myproject/shop/models.py
+++ myproject/shop/models.py
@@ -8,6 +8,9 @@ class Item(models.Model):
     price=models.PositiveIntegerField()
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return f'<{self.pk}>{self.name}'
+
 
 class Post(models.Model) :
     title = models.CharField(max_length=100)
